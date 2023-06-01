a8db4bb9552a44bc9577490ed8c586f84879caec
Edgarka1 <118747978+edgarka1@users.noreply.github.com>
2023-06-01 08:41:01 +0300

    Add files via upload

---
+++ RoomsDescription/RoomLibrary/Room.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class Room
+    {
+        double roomLenght;  // Длина помещения
+        double roomWidth;   // Ширина помещения
+
+        public double RoomLenght
+        {
+            get { return roomLenght; }
+            set { roomLenght = value; }
+        }
+
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+
+        /// <summary>
+        /// Метод вычисляет периметр помещения
+        /// </summary>
+        /// <returns>Возвращает значение периметра</returns>
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLenght + roomWidth);
+        }
+
+        /// <summary>
+        /// Метод вычисляет площадь помещения
+        /// </summary>
+        /// <returns>Возвращает значение площади</returns>
+        public double RoomArea()
+        {
+            return roomLenght * roomWidth;
+        }
+
+        /// <summary>
+        /// Метод вычисляет число квадратных метров на одного человека
+        /// </summary>
+        /// <param name="np">Число человек</param>
+        /// <returns>Возвращает число квадратных метров</returns>
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+
+        /// <summary>
+        /// Метод формирует информацию о помещении
+        /// </summary>
+        /// <returns>Возвращает строку с информацией</returns>
+        public virtual string Info()
+        {
+            return "Помещение площадью " + RoomArea() + "кв. м.";
+        }
+    }
+
+    /// <summary>
+    /// Класс "Жилая комната"
+    /// </summary>
+    public class LivingRoom : Room
+    {
+        int numWin;     // Число окон
+
+        public int NumWin
+        {
+            get { return numWin; }
+            set { numWin = value; }
+        }
+
+        /// <summary>
+        /// Метод формирует информацию о жилой комнате
+        /// </summary>
+        /// <returns>Возвращает строку с информацией</returns>
+        public override string Info()
+        {
+            return "Жилая комната площадью " + RoomArea() + "кв. м., с " + numWin + " окнами";
+        }
+    }
+
+    /// <summary>
+    /// Класс "Офис"
+    /// </summary>
+    public class Office : Room
+    {
+        int numSockets;     // Число розеток
+
+        public int NumSockets
+        {
+            get { return numSockets; }
+            set { numSockets = value; }
+        }
+
+        /// <summary>
+        /// Метод вычисляет максимально возможное число рабочих мест
+        /// </summary>
+        /// <returns>Возвращает число рабочих мест</returns>
+        public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, num);
+        }
+
+        /// <summary>
+        /// Метод формирует информацию об офисе
+        /// </summary>
+        /// <returns>Возвращает строку с информацией</returns>
+        public override string Info()
+        {
+            return "Офис на " + NumWorkplaces() + " рабочих мест";
+        }
+    }
+}
---
+++ RoomsDescription/RoomsDescription/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RoomLibrary;
+
+namespace RoomsDescription
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Room> lstRooms = new List<Room>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = new Room();
+
+            try
+            {
+                room.RoomLenght = Convert.ToDouble(TBLenghtR.Text);
+                room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+            }
+            catch (FormatException)
+            {
+                TBLenghtR.BorderBrush = Brushes.Red;
+                TBWidthR.BorderBrush = Brushes.Red;
+
+                return;
+            }
+
+            TBLenghtR.BorderBrush = Brushes.Transparent;
+            TBWidthR.BorderBrush = Brushes.Transparent;
+
+            lstRooms.Add(room);
+        }
+
+        private void BAddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            LivingRoom livingRoom = new LivingRoom();
+
+            try
+            {
+                livingRoom.RoomLenght = Convert.ToDouble(TBLenghtL.Text);
+                livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+                livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+            }
+            catch (FormatException)
+            {
+                TBLenghtL.BorderBrush = Brushes.Red;
+                TBWidthL.BorderBrush = Brushes.Red;
+                TBNumW.BorderBrush = Brushes.Red;
+
+                return;
+            }
+
+            TBLenghtL.BorderBrush = Brushes.Transparent;
+            TBWidthL.BorderBrush = Brushes.Transparent;
+            TBNumW.BorderBrush = Brushes.Transparent;
+
+            lstRooms.Add(livingRoom);
+        }
+
+        private void BAddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            Office office = new Office();
+
+            try
+            {
+                office.RoomLenght = Convert.ToDouble(TBLenghtO.Text);
+                office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+                office.NumSockets = Convert.ToInt32(TBNumS.Text);
+            }
+            catch (FormatException)
+            {
+                TBLenghtO.BorderBrush = Brushes.Red;
+                TBWidthO.BorderBrush = Brushes.Red;
+                TBNumS.BorderBrush = Brushes.Red;
+
+                return;
+            }
+
+            TBLenghtO.BorderBrush = Brushes.Transparent;
+            TBWidthO.BorderBrush = Brushes.Transparent;
+            TBNumS.BorderBrush = Brushes.Transparent;
+
+            lstRooms.Add(office);
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+
+            foreach (Room r in lstRooms)
+                ListRooms.Content += r.Info() + '\n';
+        }
+
+        private void TextBoxes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox.BorderBrush != Brushes.Transparent)
+                textBox.BorderBrush = Brushes.Transparent;
+        }
+    }
+}
